26c56a8e16dbf9e32bb4a29ff7e3fc3698910e71
Przemek Rura <przemekrura95@gmail.comgit config --global user.email przemekrura95@gmail.com>
2021-03-11 19:51:49 +0100

    add bg and link to instagram

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <div className="desc">
+        <h1>Coming soon</h1>
+        <a href="https://www.instagram.com/przemek_rura/?hl=pl">INSTAGRAM</a>
+      </div>
     </div>
   );
 }
